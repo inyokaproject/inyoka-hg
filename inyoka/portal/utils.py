4805f8e43e6465f54b5e69b51f01d390393d0c71
mitsuhiko <none@none>
2008-01-26 04:32:19 +0100

    added utils

---
+++ inyoka/portal/utils.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+"""
+    inyoka.portal.utils
+    ~~~~~~~~~~~~~~~~~~~
+
+    Utilities for the portal.
+
+    :copyright: 2007 by Benjamin Wiegand, Christopher Grebs, Armin Ronacher.
+    :license: GNU GPL, see LICENSE for more details.
+"""
+from django.http import HttpResponseRedirect
+from inyoka.utils.urls import href
+from inyoka.utils.flashing import flash
+
+
+def decor(decorator, base):
+    decorator.__name__ = base.__name__
+    decorator.__module__ = base.__module__
+    decorator.__doc__ = base.__doc__
+    return decorator
+
+
+def check_login(message=None):
+    """
+    This function can be used as a decorator to check whether the user is
+    logged in or not. Also it's possible to send the user a message if
+    hes' logged out and needs to login.
+    """
+    def _wrapper(func):
+        def decorator(*args, **kwargs):
+            req = args[0]
+            if req.user.is_authenticated():
+                return func(*args, **kwargs)
+            if message is not None:
+                flash(message)
+            args = {'next': 'http://%s%s' % (req.get_host(), req.path)}
+            return HttpResponseRedirect(href('portal', 'login', **args))
+        return decor(decorator, func)
+
+    return _wrapper
+
+
+def simple_check_login(f):
+    """
+    This function can be used as a decorator to check whether the user is
+    logged in or not.
+    If he is, the decorated function is called normally, else the login page
+    is shown without a response to the user.
+    """
+    def decorator(*args, **kwargs):
+        req = args[0]
+        if req.user.is_authenticated():
+            return f(*args, **kwargs)
+        args = {'next': 'http://%s%s' % (req.get_host(), req.path)}
+        return HttpResponseRedirect(href('portal', 'login', **args))
+
+    return decor(decorator, f)
