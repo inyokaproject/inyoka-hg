3b42cae14c8c10d8998e440f5e254eb1229beb4e
beewee <none@none>
2008-01-27 01:46:32 +0100

    small bug in movetopic

--- inyoka/forum/views.py
+++ inyoka/forum/views.py
@@ -759,8 +759,8 @@ def movetopic(request, topic_slug):
     if not have_privilege(request.user, t.forum, 'moderate'):
         return abort_access_denied()
 
-    forums = filter_invisible(request.user, Forum.objects.get_forms()
-                            .exclude(id=t.forum.id), 'read')
+    forums = filter_invisible(request.user, Forum.objects.get_forums()
+                              .exclude(id=t.forum.id), 'read')
     mapping = dict((x.id, x) for x in forums)
     if not mapping:
         return abort_access_denied(request)
