275f4fd15383e9b972d34ac81796c8ea11214a7f
tux21b <none@none>
2008-02-05 15:16:21 +0100

    forgot some files :/

---
+++ inyoka/forum/search.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+"""
+    inyoka.forum.search
+    ~~~~~~~~~~~~~~~~~~~
+
+    Search interfaces for the forum.
+
+    :copyright: Copyright 2008 by Christoph Hack.
+    :license: GNU GPL, see LICENSE for more details.
+"""
+from inyoka.forum.acl import get_privileges
+from inyoka.forum.models import Post, Forum
+from inyoka.utils.urls import url_for
+from inyoka.utils.search import search, SearchAdapter
+
+
+class ForumSearchAuthDecider(object):
+    """Decides whetever a user can display a search result or not."""
+
+    def __init__(self, user):
+        privs = get_privileges(user, Forum.objects.all())
+        self.privs = dict((key, priv['read']) for key, priv in privs.iteritems())
+
+    def __call__(self, auth):
+        # TODO: Hide hidden topics
+        return self.privs.get(auth[0], False)
+
+
+class ForumSearchAdapter(SearchAdapter):
+    type_id = 'f'
+    auth_decider = ForumSearchAuthDecider
+
+    def store(self, post_id):
+        post = Post.objects.select_related(2).get(id=post_id)
+        search.store(
+            component='f',
+            uid=post.id,
+            title=post.topic.title,
+            user=post.author_id,
+            date=post.pub_date,
+            collapse=post.topic_id,
+            category=[p.slug for p in post.topic.forum.parents] + \
+                [post.topic.forum.slug],
+            auth=[post.topic.forum_id, post.topic.hidden],
+            text=post.text
+        )
+
+    def recv(self, post_id):
+        post = Post.objects.select_related(2).get(id=post_id)
+        return {
+            'title': post.topic.title,
+            'user': post.author,
+            'date': post.pub_date,
+            'url': url_for(post),
+            'component': u'Forum',
+            'highlight': True
+        }
+search.register(ForumSearchAdapter())
