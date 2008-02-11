f251bea820ffe7da387b3c08ca158f066d06f633
beewee <none@none>
2008-02-11 17:53:31 +0100

    updated PrivilegeBox

--- inyoka/static/js/PrivilegeBox.js
+++ inyoka/static/js/PrivilegeBox.js
@@ -1,6 +1,4 @@
 (function() {
-  var mapping = {}
-
   PrivilegeBox = function(container, forums, privileges) {
     var self = this;
     this.container = $(container);
@@ -14,19 +12,14 @@
           var privileges_select = $('<select multiple="multiple" class="forum_privileges" size="10"></select>')
             .attr('id', id.slice(1))
             .attr('name', id.slice(1))
-            .change(function() {
-              mapping[$(select.find(':selected')[0]).val()] = $(this).find(':selected');
-            });
           $.each(privileges, function(i, perm) {
             privileges_select.append($('<option></option>').val(perm[0]).text(perm[1]));
           });
-          privileges_select.val(mapping[$(this).val()]);
           self.container.append(privileges_select);
         };
       });
     $.each(forums, function(i, forum) {
       var [slug, name, perms] = forum;
-      mapping[slug] = perms;
       select.append($('<option></option>').val(slug).text(name));
     });
     this.container.html('');
