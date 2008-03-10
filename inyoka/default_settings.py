774deb35b1495bb5b8c32f16bfccd2c2458f0db6
maix <none@none>
2008-03-10 22:54:40 +0100

    removed TRAC_* settings from default conf

--- inyoka/default_settings.py
+++ inyoka/default_settings.py
@@ -33,11 +33,6 @@ DATABASE_NAME = DATABASE_USER = DATABASE_HOST = DATABASE_PORT = ''
 # mysql only
 DATABASE_ENGINE = 'mysql'
 
-# if we are in debug mode we issue tickets into a trac
-TRAC_URL = 'http://trac.staging.ubuntuusers.de/'
-TRAC_USERNAME = 'logger'
-TRAC_PASSWORD = None
-
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be avilable on all operating systems.
