eaa4bb2db23d0ce714589e9f787897a7b2f19663
tux21b <none@none>
2008-03-13 01:37:32 +0100

    delete documents from the search index again

--- inyoka/utils/search.py
+++ inyoka/utils/search.py
@@ -224,7 +224,10 @@ class SearchSystem(object):
         self._connection = None
 
     def index(self, component, docid):
-        self.adapters[component].store(docid)
+        try:
+            self.adapters[component].store(docid)
+        except ObjectDoesNotExist:
+            self.delete(component, docid)
 
     def queue(self, component, docid):
         from inyoka.portal.models import SearchQueue
@@ -361,6 +364,10 @@ class SearchSystem(object):
 
         connection = self.get_connection(True)
         connection.replace_document('Q%s:%d' % full_id, doc)
+
+    def delete(self, component, docid):
+        full_id = (component.lower(), docid)
+        self.get_connection(True).delete_document('Q%s:%d' % full_id)
 
     def flush(self):
         if self._connection:
