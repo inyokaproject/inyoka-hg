8cdab9caf7f1c9c55c881febe5256302ce2c111b
EnTeQuAk <none@none>
2008-03-22 13:45:16 +0100

    fixed migrations

--- inyoka/migrations.py
+++ inyoka/migrations.py
@@ -25,7 +25,6 @@ from inyoka.conf import settings
 
 SQL_FILES = join(dirname(__file__), 'sql')
 
-
 def execute_script(con, name):
     """Execute a script on a connectable."""
     f = file(join(SQL_FILES, name))
@@ -52,6 +51,48 @@ def fix_ikhaya_icon_relation_definition(m):
         alter table ikhaya_article modify column icon_id integer;
     ''')
 
+
+def add_skype_and_sip(m):
+    """
+    This migration added support for skype and SIP profile fields.
+    """
+    m.engine.execute('''
+        alter table portal_user
+            add column skype varchar(200) not null after yim,
+            add column wengophone varchar(200) not null after skype,
+            add column sip varchar(200) not null after wengophone;
+    ''')
+
+
+def add_subscription_notified_and_forum(m):
+    """
+    This migration added two fields to the subscription table.
+    """
+    m.engine.execute('''
+        begin;
+        alter table portal_subscription
+            add column forum_id integer null after topic_id,
+            add column notified bool not null after wiki_page_id;
+        create index portal_subscription_forum_id
+            on portal_subscription (forum_id);
+        commit;
+        alter table portal_subscription
+            add constraint forum_id_refs_id_7009f990
+                foreign key forum_id_refs_id_7009f990 (forum_id)
+                references forum_forum (id);
+    ''')
+
+
+def add_wiki_revision_change_date_index(m):
+    """
+    This revision added an index on the change date
+    """
+    m.engine.execute('''
+        alter table wiki_revision
+            add index wiki_revision_change_date(change_date);
+    ''')
+
+
 def fix_sqlalchemy_forum(m):
     """
     This migration alters some forum tables to match the new
@@ -63,5 +104,9 @@ def fix_sqlalchemy_forum(m):
     ''')
 
 
-MIGRATIONS = [create_initial_revision, fix_ikhaya_icon_relation_definition,
-             fix_sqlalchemy_forum]
+
+MIGRATIONS = [
+    create_initial_revision, fix_ikhaya_icon_relation_definition,
+    add_skype_and_sip, add_subscription_notified_and_forum,
+    add_wiki_revision_change_date_index, fix_sqlalchemy_forum
+]
