b7a99fab927fcc9dfe219a4ffa7090ea7eb1bd6a
apollo13 <none@none>
2008-05-15 14:43:35 +0200

    fixed storage access

--- inyoka/utils/mail.py
+++ inyoka/utils/mail.py
@@ -54,4 +54,4 @@ def is_blocked_host(email_or_host):
     is configurable in the admin panel.
     """
     host = email_or_host.rsplit('@', 1)[-1]
-    return host in storage['blocked_hosts'].split('\n')
+    return host in storage.get('blocked_hosts','').split('\n')
