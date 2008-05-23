fd5f88c38533ffa8d83ce72fc626d6e23c32a978
beewee <none@none>
2008-05-23 12:02:17 +0200

    post converter now uses post text table too

--- inyoka/scripts/converter/converter.py
+++ inyoka/scripts/converter/converter.py
@@ -32,7 +32,8 @@ from inyoka.wiki.parser.transformers import AutomaticParagraphs, Transformer
 from inyoka.forum.models import Privilege, Attachment, Topic, \
     Poll, Forum, topic_table as sa_topic_table, forum_table as \
     sa_forum_table, post_table as sa_post_table, poll_vote_table as \
-    sa_poll_vote_table, poll_option_table as sa_poll_option_table
+    sa_poll_vote_table, poll_option_table as sa_poll_option_table, \
+    post_text_table as sa_post_text_table
 from inyoka.portal.models import PrivateMessage, PrivateMessageEntry, \
     Subscription, StaticFile, StaticPage
 from inyoka.ikhaya.models import Article, Category
@@ -424,12 +425,15 @@ def convert_forum():
         session.execute(sa_post_table.insert(values={
             'id':            row[post_table.c.post_id],
             'topic_id':      row[post_table.c.topic_id],
-            'text':          text,
             'author_id':     row[post_table.c.poster_id],
             'pub_date':      datetime.fromtimestamp(row[post_table.c.post_time]),
-            'rendered_text': '',
             'hidden':        False
         }))
+        session.execute(sa_post_text_table.insert(values={
+            'id':            row[post_table.c.post_id],
+            'text':          text,
+            'rendered_text': '',
+        })
         session.commit()
 
     print 'fixing forum references'
