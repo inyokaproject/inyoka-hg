9f88d724911391ab119f5336af4ebb679fb1c1e6
EnTeQuAk <none@none>
2008-06-28 13:49:16 +0200

    removed pocoo dependency from count loc ;)

--- extra/count_loc.py
+++ extra/count_loc.py
@@ -4,37 +4,40 @@
     Source line counter
     ~~~~~~~~~~~~~~~~~~~
 
-    Count the lines of code in pocoo, colubrid and jinja.
-    Requires an svn checkout.
+    Count the lines of inyoka.
 
-    :copyright: 2006-2007 by Armin Ronacher.
+    :copyright: 2006-2007 by Armin Ronacher, 2008 by Christopher Grebs.
     :license: GNU GPL, see LICENSE for more details.
 """
-from pocoo.utils.path import path
+import os
+from os import path
+
 
 def main(root, search):
     LOC = 0
 
-    root = path(root).realpath()
-    offset = len(root) + 1
+    root = path.abspath(root)
 
     print '+%s+' % ('=' * 78)
     print '| Lines of Code %s |' % (' ' * 62)
     print '+%s+' % ('=' * 78)
 
     for folder in search:
-        folder = path(root).joinpath(folder).realpath()
-        for fn in folder.walk():
-            if fn.endswith('.py') or fn.endswith('.js') or fn.endswith('.html'):
-                try:
-                    fp = file(fn)
-                    lines = sum(1 for l in fp.read().splitlines() if l.strip())
-                except:
-                    print '%-70sskipped' % fn
-                else:
-                    LOC += lines
-                    print '| %-68s %7d |' % (fn[offset:], lines)
-                fp.close()
+        off = 78/2 - len(folder)
+        print '+%s  %s  %s+' % ('-' * off, folder, '-' * (off +1))
+        folder = path.join(root, folder)
+        for base, dirname, files in os.walk(folder):
+            for fn in files:
+                if fn.endswith('.py') or fn.endswith('.js') or fn.endswith('.html'):
+                    try:
+                        fp = file(path.join(base, fn))
+                        lines = sum(1 for l in fp.read().splitlines() if l.strip())
+                    except:
+                        print '%-70sskipped' % fn
+                    else:
+                        LOC += lines
+                        print '| %-68s %7d |' % (fn, lines)
+                    fp.close()
 
     print '+%s+' % ('-' * 78)
     print '| Total Lines of Code: %55d |' % LOC
