ebfee334e838e3cb2011f3deb870106dfdc1e589
apollo13 <none@none>
2008-07-17 01:21:22 +0200

    stupid me :(

--- easy_uninstall.py
+++ easy_uninstall.py
@@ -11,10 +11,13 @@ import glob, sys, os, shutil
 eggs = glob.glob('%s*' % sys.argv[1])
 for egg in eggs:
     if egg.endswith('.egg-link'):
-        data = file(egg,'r').readlines()[0]
+        data = file(egg,'r').readlines()[0].strip()
     else:
         data = './%s' % egg
-    file('easy-install.pth', 'w').write(file('easy-install.pth', 'r').read().replace('%s\n' % data, ''))
+    print data
+    pth_content = file('easy-install.pth', 'r').read().replace('%s\n' % data, '')
+    print pth_content
+    file('easy-install.pth', 'w').write(pth_content)
     try:
         os.remove(egg)
         shutil.rmtree(egg, True)
