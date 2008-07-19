1c78db504819ebd98e7395c12f802590096311d8
tux21b <none@none>
2008-07-19 10:50:30 +0200

    fixed another bug

--- inyoka/portal/forms.py
+++ inyoka/portal/forms.py
@@ -346,7 +346,7 @@ class UserCPProfileForm(forms.Form):
         return lat, long
 
     def clean_email(self):
-        email = self.cleaned_data.get('email', None).strip()
+        email = self.cleaned_data.get('email', '').strip()
         if not email:
             raise forms.ValidationError(u'Keine Email-Adresse angegeben!')
         try:
