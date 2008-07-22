12f17181491236176b53849b4b247c3c5b1d50b6
tux21b <none@none>
2008-07-23 01:10:45 +0200

    something is br0ken here...

--- inyoka/application.py
+++ inyoka/application.py
@@ -9,6 +9,7 @@
     :license: GNU GPL.
 """
 from django.core.handlers.wsgi import WSGIHandler
+from django.core import urlresolvers
 from inyoka.conf import settings
 from inyoka.utils.http import PageNotFound, DirectResponse, TemplateResponse
 from inyoka.utils.logger import logger
@@ -97,7 +98,10 @@ class InyokaHandler(WSGIHandler):
                             return response
                     raise
             except PageNotFound, e:
-                callback, param_dict = request.resolver.resolve404()
+                if resolver is None:
+                    urlconf = getattr(request, "urlconf", settings.ROOT_URLCONF)
+                    resolver = urlresolvers.RegexURLResolver(r'^/', urlconf)
+                callback, param_dict = resolver.resolve404()
                 return callback(request, **param_dict)
             except DirectResponse, e:
                 return e.response
