46cf2d67d1edd1b85bb5e7e6a4eec4032e44f04d
maix <none@none>
2008-08-04 23:08:16 +0200

    added more fields to the blog suggest form (#2599)

--- inyoka/planet/forms.py
+++ inyoka/planet/forms.py
@@ -15,7 +15,12 @@ class SuggestBlogForm(forms.Form):
     """Form to suggest a new blog url for the planet."""
     name = forms.CharField(label=u'Name des Blogs')
     url =  forms.URLField(label=u'URL')
+    feed_url =  forms.URLField(label=u'Feed-URL', required=False)
     description = forms.CharField(label=u'Beschreibung',
         widget=forms.Textarea,
         help_text=(u'Die Beschreibung dient dem Ikhaya-Team '
               u'sich einen besseren Überblick zu verschaffen.'))
+    mine = forms.BooleanField(label=u'Dieser Blog ist mein eigener',
+                              required=False)
+    contact_email = forms.EmailField(label=u'E-Mail-Adresse des Autors des '
+                                           u'Blogs', required=False)
