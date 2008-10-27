a43e91f05a9153887f77c938cd2fd1a011f0e009
EnTeQuAk <none@none>
2008-10-27 18:47:26 +0100

    fixed an error that edited attachments are not shown correclty (because old attachments are used instead of new ones)

--- inyoka/wiki/models.py
+++ inyoka/wiki/models.py
@@ -441,7 +441,7 @@ class PageManager(models.Manager):
               from wiki_attachment a, wiki_revision r, wiki_page p
              where r.page_id = p.id and r.attachment_id = a.id and
                    p.name = %s and not r.deleted
-          group by a.file;
+          group by a.file order by r.id desc;
         ''', [page_name])
         row = cursor.fetchone()
         if row:
