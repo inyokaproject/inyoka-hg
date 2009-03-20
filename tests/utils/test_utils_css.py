1946bb502ac6b12d68f5e4da3797eccf52718fec
apollo13 <none@none>
2009-03-20 15:30:26 +0100

    remove cssutils warning messages

--- tests/utils/test_utils_css.py
+++ tests/utils/test_utils_css.py
@@ -9,7 +9,13 @@
     :copyright: Copyright 2009 by Christopher Grebs.
     :license: GNU GPL.
 """
+# Disable cssutils logging messages for now
+import logging
+log = logging.getLogger('CSSUTILS')
+log.setLevel(logging.ERROR)
+
 from inyoka.utils.css import filter_style
+
 
 #TODO: add more security problems here!
 
