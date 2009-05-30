dac029a95e45014178e21b1f814c766b4c99594d
apollo13 <none@none>
2009-05-30 21:37:48 +0200

    backout the previous checkin

--- inyoka/forum/models.py
+++ inyoka/forum/models.py
@@ -80,6 +80,7 @@ UBUNTU_VERSIONS = [
 UBUNTU_DISTROS = {
     'ubuntu': 'Ubuntu',
     'kubuntu': 'Kubuntu',
+    'kubuntu-kde4': u'Kubuntu (KDE 4)',
     'xubuntu': 'Xubuntu',
     'server': 'Server',
     'edubuntu': 'Edubuntu',
