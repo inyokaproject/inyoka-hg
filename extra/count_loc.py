ce9ae528c98b03d180869e067734d516453d7364
EnTeQuAk <none@none>
2009-07-04 13:17:45 +0200

    fixed count_log.py a bit

--- extra/count_loc.py
+++ extra/count_loc.py
@@ -6,7 +6,7 @@
 
     Count the lines of inyoka.
 
-    :copyright: 2006-2007 by Armin Ronacher, 2008 by Christopher Grebs.
+    :copyright: 2006-2007 by Armin Ronacher, 2008-2009 by Christopher Grebs.
     :license: GNU GPL, see LICENSE for more details.
 """
 import os
@@ -25,21 +25,23 @@ def main(root, search):
 
     for folder in search:
         off = 78/2 - len(folder)
-        print '+%s  %s  %s+' % ('-' * off, folder, '-' * (off +1))
+        print '+%s  %s  %s+' % ('-' * off, folder, '-' * (off + 1))
         folder = path.join(root, folder)
+        folder_locs = LOC
         for base, dirname, files in os.walk(folder):
             for fn in files:
                 fn = path.join(base, fn)
                 if fn.endswith('.py') or fn.endswith('.js') or fn.endswith('.html'):
                     try:
-                        fp = file(fn)
+                        fp = open(fn)
                         lines = sum(1 for l in fp.read().splitlines() if l.strip())
                     except:
                         print '%-70sskipped' % fn
                     else:
                         LOC += lines
-                        print '| %-68s %7d |' % (fn[offset:], lines)
+                        print '| %-66s %7d   |' % (fn[offset:], lines)
                     fp.close()
+        print '+-- %-56s Gesamt: %7d --+' % (' ' * off, LOC - folder_locs)
 
     print '+%s+' % ('-' * 78)
     print '| Total Lines of Code: %55d |' % LOC
