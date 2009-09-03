779f6e9081556d99ea0ee9f50837205e3e322410
Oskar Hahn <ostcar@web.de>
2009-09-03 19:32:13 +0200

    reindex the topic, when the ubuntu_version of a topic changed

--- inyoka/forum/views.py
+++ inyoka/forum/views.py
@@ -535,8 +535,10 @@ def edit(request, forum_slug=None, topic_slug=None, post_id=None,
             topic = Topic(forum_id=forum.id, author_id=request.user.id)
         if newtopic or firstpost:
             topic.title = d['title']
-            topic.ubuntu_distro = d.get('ubuntu_distro')
-            topic.ubuntu_version = d.get('ubuntu_version')
+            if topic.ubuntu_distro != d.get('ubuntu_distro') or topic.ubuntu_version != d.get('ubuntu_version'):
+                topic.ubuntu_distro = d.get('ubuntu_distro')
+                topic.ubuntu_version = d.get('ubuntu_version')
+                topic.reindex()
             if check_privilege(privileges, 'sticky'):
                 topic.sticky = d['sticky']
             if check_privilege(privileges, 'create_poll'):
