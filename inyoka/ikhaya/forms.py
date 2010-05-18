a3eafe9bd32c9108231cce3b23d366435037f5f9
maix <none@none>
2010-05-18 22:24:22 +0200

    s/kommentar_nummer/kommentarnummer/ (http://forum.ubuntuusers.de/post/2493357/)

--- inyoka/ikhaya/forms.py
+++ inyoka/ikhaya/forms.py
@@ -22,6 +22,6 @@ class SuggestArticleForm(forms.Form):
 class EditCommentForm(forms.Form):
     text = forms.CharField(label=u'Text', widget=forms.Textarea,
              help_text=u'Um dich auf einen anderen Kommentar zu beziehen, '
-               u'kannst du <em>@kommentar_nummer</em> verwenden.<br />'
+               u'kannst du <code>@kommentarnummer</code> verwenden.<br />'
                u'Dies wird automatisch eingefügt, wenn du bei einem Beitrag '
                u'auf „Antworten“ klickst.')
