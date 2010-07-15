f669aa643c45dfdbddc9b36d38f9c7f11c3119a8
Christopher Grebs <cg@webshox.org>
2010-07-15 16:37:34 +0200

    south is now in bootstrap

--- make-bootstrap.py
+++ make-bootstrap.py
@@ -94,6 +94,7 @@ def after_install(options, home_dir):
     easy_install('Django==1.1.2', home_dir)
     easy_install('python-memcached', home_dir)
     easy_install('cssutils==0.9.7b3', home_dir)
+    easy_install('South', home_dir)
     xapian_install(os.path.abspath(home_dir))
     pil_install(os.path.abspath(home_dir))
 """
