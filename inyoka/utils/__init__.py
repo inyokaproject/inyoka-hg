b3240dc7fc46e82f951691c9190fc19d2626f9f8
Christopher Grebs <cg@webshox.org>
2010-07-28 20:59:33 +0200

    implemented better estimation of how many pages we got

--- inyoka/utils/__init__.py
+++ inyoka/utils/__init__.py
@@ -8,7 +8,7 @@
     :copyright: Copyright 2008 by Marian Sigler.
     :license: GNU GPL.
 """
-
+import math
 from os import path, rmdir, mkdir
 import cPickle
 try:
@@ -70,3 +70,41 @@ class classproperty(object):
     def __get__(self, desc, cls):
         value = self.func(cls)
         return value
+
+
+def get_significant_digits(value, lower, upper):
+    """Get the significant digits of value which are constrained by the
+    (inclusive) lower and upper bounds.
+
+    If there are no significant digits which are definitely within the
+    bounds, exactly one significant digit will be returned in the result.
+
+    >>> get_significant_digits(15, 15, 15)
+    15
+    >>> get_significant_digits(4777, 208, 6000)
+    5000
+    >>> get_significant_digits(9, 9, 100)
+    9
+
+    """
+    assert(lower <= value)
+    assert(value <= upper)
+    diff = upper - lower
+
+    # Get the first power of 10 greater than the difference.
+    # This corresponds to the magnitude of the smallest significant digit.
+    if diff == 0:
+        pos_pow_10 = 1
+    else:
+        pos_pow_10 = int(10 ** math.ceil(math.log10(diff)))
+
+    # Special case for situation where we don't have any significant digits:
+    # get the magnitude of the most significant digit in value.
+    if pos_pow_10 > value:
+        if value == 0:
+            pos_pow_10 = 1
+        else:
+            pos_pow_10 = int(10 ** math.floor(math.log10(value)))
+
+    # Return the value, rounded to the nearest multiple of pos_pow_10
+    return ((value + pos_pow_10 // 2) // pos_pow_10) * pos_pow_10
