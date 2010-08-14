a7e55a17a2729e8cdfbc8e161aae5cfd43627f83
Christopher Grebs <cg@webshox.org>
2010-08-14 14:02:53 +0200

    Save more advanced frame information to the traceback database

--- inyoka/utils/mongolog.py
+++ inyoka/utils/mongolog.py
@@ -92,6 +92,50 @@ def get_exception_message(exc_info):
         return traceback.format_exception_only(*exc_info[:2])[0].strip()
 
 
+def get_traceback_frames(exc_info):
+    frames = []
+    tb = exc_info[2]
+    while tb is not None:
+        # support for __traceback_hide__ which is used by a few libraries
+        # to hide internal frames.
+        if tb.tb_frame.f_locals.get('__traceback_hide__'):
+            tb = tb.tb_next
+            continue
+        filename = tb.tb_frame.f_code.co_filename
+        function = tb.tb_frame.f_code.co_name
+        lineno = tb.tb_lineno - 1
+        module_name = tb.tb_frame.f_globals.get('__name__')
+        frames.append({
+            'filename': filename,
+            'function': function,
+            'lineno': lineno + 1,
+            'vars': tb.tb_frame.f_locals.items(),
+            'id': id(tb),
+        })
+        tb = tb.tb_next
+
+    if not frames:
+        frames = [{
+            'filename': '<unknown>',
+            'function': '?',
+            'lineno': '?',
+        }]
+
+    return frames
+
+
+def serialize_as_much_as_possible(frame):
+    result = {}
+    vars = [(key, [repr(v) for v in value] if '__iter__' in dir(value) else repr(value))
+                  for key, value in frame['vars']]
+
+    result['vars'] = vars
+    for item in ('filename', 'function', 'lineno', 'id'):
+        result[item] = frame[item]
+
+    return result
+
+
 class MongoHandler(logging.Handler):
     """ Custom log handler
 
@@ -131,6 +175,7 @@ class MongoHandler(logging.Handler):
                     record.exc_text = fmt.formatException(record.exc_info)
             record.asctime = fmt.formatTime(record, fmt.datefmt)
 
+            frames = get_traceback_frames(record.exc_info)
             msg = {
                 'hash': get_record_hash(record),
                 'revision': INYOKA_REVISION,
@@ -141,7 +186,8 @@ class MongoHandler(logging.Handler):
                 'message': record.message,
                 'asctime': record.asctime,
                 'exc_text': record.exc_text,
-                'occured': 0
+                'occured': 1,
+                'frame': serialize_as_much_as_possible(frames[-1]),
             }
 
             collection.save(msg)
