3c93af69d44c08fc5c9ca773eda563307305def0
Christopher Grebs <cg@webshox.org>
2010-08-15 18:22:17 +0200

    surround the whole search displaying block with that try block to catch DatabaseModificationError, should fix recently occured traceback hashed 1ba4026c450bb5f670ba7c6ffe62e676

--- inyoka/utils/search.py
+++ inyoka/utils/search.py
@@ -283,14 +283,11 @@ class SearchSystem(object):
                 enq.set_query(qry)
 
                 mset = enq.get_mset(offset, per_page, per_page, None, auth)
+                return SearchResult(mset, enq, qry, page, per_page, self.adapters)
             except xapian.DatabaseModificationError:
                 time.sleep(0.1)
                 connection.reopen()
                 _connection_attemts += 1
-            else:
-                break
-
-        return SearchResult(mset, enq, qry, page, per_page, self.adapters)
 
     def store(self, **data):
         doc = xapian.Document()
