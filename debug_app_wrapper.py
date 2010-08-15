b960c91ca53d6740aa3d31a2c531331cb657f042
Christopher Grebs <cg@webshox.org>
2010-08-15 12:06:53 +0200

    add sample gunicorn config with working core reloading, start it with gunicorn -c gunicorn_conf.py debug_app_wrapper:app

---
+++ debug_app_wrapper.py
@@ -0,0 +1,14 @@
+def make_app():
+    import inyoka.utils.http
+    from inyoka.conf import settings
+    from inyoka.application import application, StaticDomainHandler
+    from werkzeug import DebuggedApplication
+    app = application
+    app = StaticDomainHandler(app)
+    if settings.DEBUG:
+        app = DebuggedApplication(app, evalex=settings.ENABLE_DEBUGGER)
+    if settings.DEBUG_LEAK:
+        gc.set_debug(gc.DEBUG_SAVEALL)
+    return app
+
+app = make_app()
