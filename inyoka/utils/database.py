15c39c54048875b97ded91e3de4f203f33c74ac1
Christopher Grebs <cg@webshox.org>
2010-08-18 13:36:16 +0200

    started to define Model class for declarative usage, use dbapi unicode conversion as it's safe to do that right now (all bugs resolved), refs #52

--- inyoka/utils/database.py
+++ inyoka/utils/database.py
@@ -14,13 +14,16 @@
     :copyright: (c) 2007-2010 by the Inyoka Team, see AUTHORS for more details.
     :license: GNU GPL, see LICENSE for more details.
 """
+from sqlalchemy import orm
 from sqlalchemy import MetaData, create_engine
 from sqlalchemy.orm import scoped_session, create_session
 from sqlalchemy.pool import NullPool
+from sqlalchemy.util import to_list
+from sqlalchemy.ext.declarative import declarative_base, _declarative_constructor
 from inyoka.conf import settings
 
 rdbm = 'mysql'
-extra = '?charset=utf8&use_unicode=0'
+extra = '?charset=utf8&use_unicode=1'
 if 'postgres' in settings.DATABASE_ENGINE:
     rdbm = 'postgres'
     extra = ''
@@ -28,12 +31,79 @@ if 'postgres' in settings.DATABASE_ENGINE:
 engine = create_engine('%s://%s:%s@%s/%s%s' % (
     rdbm, settings.DATABASE_USER, settings.DATABASE_PASSWORD,
     settings.DATABASE_HOST, settings.DATABASE_NAME, extra
-), pool_recycle=25, convert_unicode=True, echo=False,
+), pool_recycle=25, echo=False,
    poolclass=NullPool)
 metadata = MetaData(bind=engine)
 
 session = scoped_session(lambda: create_session(engine,
-    autoflush=True, transactional=True))
+    autoflush=True, autocommit=False))
+
+
+def mapper(model, table, **options):
+    """A mapper that hooks in standard extensions."""
+    extensions = to_list(options.pop('extension', None), [])
+    options['extension'] = extensions
+    # automatically register the model to the session
+    old_init = getattr(model, '__init__', lambda s: None)
+    def register_init(self, *args, **kwargs):
+        old_init(self, *args, **kwargs)
+        session.add(self)
+    model.__init__ = register_init
+    return orm.mapper(model, table, **options)
+
+
+class Query(orm.Query):
+    """Default query class."""
+
+    def cached(self, key, timeout=None):
+        """Return a query result from the cache or execute the query again"""
+        from inyoka.utils.cache import cache
+        data = cache.get(key)
+        if data is None:
+            data = self.all()
+            cache.set(key, data, timeout=timeout)
+        data = list(self.merge_result(data, load=False))
+        return data
+
+    def lightweight(self, deferred=None, lazy=None):
+        """Send a lightweight query which deferes some more expensive
+        things such as comment queries or even text and parser data.
+        """
+        args = map(db.lazyload, lazy or ()) + map(db.defer, deferred or ())
+        return self.options(*args)
+
+
+class ModelBase(object):
+    """Internal baseclass for all models.  It provides some syntactic
+    sugar and maps the default query property.
+
+    We use the declarative model api from sqlalchemy.
+    """
+
+    def __eq__(self, other):
+        equal = True
+        if type(other) != type(self):
+            return False
+        for key in type(self)._sa_class_manager.mapper.columns.keys():
+            if getattr(self, key) != getattr(other, key):
+                equal = False
+                break
+        return equal
+
+    def __repr__(self):
+        attrs = []
+        dict_ = type(self)._sa_class_manager.mapper.columns.keys()
+        for key in dict_:
+            if not key.startswith('_'):
+                attrs.append((key, getattr(self, key)))
+        return self.__class__.__name__ + '(' + ', '.join(x[0] + '=' +
+                                            repr(x[1]) for x in attrs) + ')'
+
+
+# configure the declarative base
+Model = declarative_base(name='Model', cls=ModelBase,
+    mapper=mapper, metadata=metadata)
+Model.query = session.query_property(Query)
 
 
 if settings.DATABASE_DEBUG:
