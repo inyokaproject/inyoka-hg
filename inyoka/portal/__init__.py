cf6fd03ad0cae3a0bd567e0f5663a007692c1430
Christopher Grebs <cg@webshox.org>
2010-08-19 13:32:40 +0200

    removed bad import from inyoka.portal.__init__

--- inyoka/portal/__init__.py
+++ inyoka/portal/__init__.py
@@ -6,5 +6,3 @@
     :copyright: (c) 2007-2010 by the Inyoka Team, see AUTHORS for more details.
     :license: GNU GPL, see LICENSE for more details.
 """
-# publish search extensions
-from inyoka.portal import search
