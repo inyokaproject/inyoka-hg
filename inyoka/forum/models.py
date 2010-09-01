44bad9b6cbdbca8c241f2c4391626da130b31e2c
Christopher Grebs <cg@webshox.org>
2010-09-01 12:44:43 +0200

    reenable django_models import in forum.models

--- inyoka/forum/models.py
+++ inyoka/forum/models.py
@@ -37,7 +37,7 @@ from inyoka.forum.acl import filter_invisible
 from inyoka.forum.compat import SAUser
 
 # Import Django models here so that South can find them
-#from inyoka.forum.django_models import *
+from inyoka.forum.django_models import *
 
 
 # initialize PIL to make Image.ID available
