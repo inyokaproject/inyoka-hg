06a8701b58a6989b86c85088693fefe5b5df870d
Christopher Grebs <cg@webshox.org>
2010-09-10 18:33:10 +0200

    don't include missing { warnings in jslint check as those are often not correct and we don't use { everywhere we could

--- extra/jslint-check.js
+++ extra/jslint-check.js
@@ -24,7 +24,6 @@ for (var file in files) {
     "Use '!==' to compare with 'null'.": true,
     "Expected an assignment or function call and instead saw an expression.": true,
     "Expected a 'break' statement before 'case'.": true
-
   };
 
   var e = JSLINT.errors, found = 0, w;
@@ -32,7 +31,7 @@ for (var file in files) {
   for ( var i = 0; i < e.length; i++ ) {
     w = e[i];
 
-    if ( !ok[ w.reason ] ) {
+    if ( !ok[ w.reason ] && (w.reason.indexOf("Expected '{' and instead saw"))) {
       found++;
       print( "\n" + w.evidence + "\n" );
       print( "    Problem at line " + w.line + " character " + w.character + ": " + w.reason );
