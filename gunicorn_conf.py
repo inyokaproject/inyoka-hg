0752b15e0c5641bccb5fc0f0c04fbb747fa0d780
Christopher Grebs <cg@webshox.org>
2010-10-01 15:05:21 +0200

    switch to more proper wsgi gevent loop

--- gunicorn_conf.py
+++ gunicorn_conf.py
@@ -24,7 +24,7 @@ elif _hostname == 'dongo':
     workers = 16
 
 
-worker_class = 'egg:gunicorn#gevent'
+worker_class = 'egg:gunicorn#gevent_pywsgi'
 worker_connections = 1000
 timeout = 30
 keepalive = 2
