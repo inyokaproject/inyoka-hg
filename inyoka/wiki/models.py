b9ee30ecd86e285f339f6029a65414c46127bea8
Florian Apolloner <apollo13@apolloner.eu>
2010-10-17 19:18:43 +0200

    WTF

--- inyoka/wiki/models.py
+++ inyoka/wiki/models.py
@@ -557,7 +557,7 @@ class PageManager(models.Manager):
                 displays in the revision log if the `note` is not changed to
                 something reasonable.
 
-            remote_addrblub
+            remote_addr
                 The remote address of the user that created this page.  Either
                 remote_addr or user is required.  This decision was made so
                 that no confusion comes up when creating page objects in the
