5c49ade1d9f528a0b27d300e4b81c4e8364abdd8
Christopher Grebs <cg@webshox.org>
2010-11-03 22:54:05 +0100

    Small improvement to the forum edit template.  The title and ubuntu version entries are now next to each other rather than beneath each other

--- inyoka/static/js/forum.js
+++ inyoka/static/js/forum.js
@@ -169,16 +169,16 @@ $(function() {
         version: version_str
       }, function(data) {
         if (data.class_ == 'unstable') {
-          $('span#version_info').attr('class', 'unstable')
+          $('p#version_info').attr('class', 'unstable')
             .html(with_link(text_unstable, data));
         } else if (data.lts) {
-          $('span#version_info').attr('class', 'lts')
+          $('p#version_info').attr('class', 'lts')
             .html(with_link(text_lts, data));
         } else if (data.current) {
-          $('span#version_info').attr('class', 'current')
+          $('p#version_info').attr('class', 'current')
             .html(with_link(text_current, data));
         } else {
-          $('span#version_info').attr('class', '').text('');
+          $('p#version_info').attr('class', '').text('');
         }
       });
 
