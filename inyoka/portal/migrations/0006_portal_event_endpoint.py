7314e7109ab1ba60786ad7f4058c42e350043557
Christopher Grebs <cg@webshox.org>
2010-11-04 02:39:32 +0100

    reindent

--- inyoka/portal/migrations/0006_portal_event_endpoint.py
+++ inyoka/portal/migrations/0006_portal_event_endpoint.py
@@ -7,7 +7,7 @@ from django.db import models
 class Migration(SchemaMigration):
 
     def forwards(self, orm):
-        
+
         # Deleting field 'Event.duration'
         db.delete_column('portal_event', 'duration')
 
@@ -19,7 +19,7 @@ class Migration(SchemaMigration):
 
 
     def backwards(self, orm):
-        
+
         # Adding field 'Event.duration'
         db.add_column('portal_event', 'duration', self.gf('django.db.models.fields.DateTimeField')(null=True, blank=True), keep_default=False)
 
